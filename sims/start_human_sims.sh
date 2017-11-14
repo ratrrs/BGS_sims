9d6572c68966b4a26e2199327e5d8d01fa1bc2ce
Markus Stetter <markusstetter@Markuss-iMac.local>
2017-11-14 15:44:52 -0800

    update

--- sims/start_human_sims.sh
+++ sims/start_human_sims.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-#SBATCH -D /home/mstetter/popgen_maize_sim/
-#SBATCH -o /home/mstetter/popgen_maize_sim/logs/simulation_out-%j.txt
-#SBATCH -e /home/mstetter/popgen_maize_sim/logs/simulation_stderr-%j.txt
+#SBATCH -D /home/mstetter/BGS_sims/
+#SBATCH -o /home/mstetter/BGS_sims/logs/simulation_out-%j.txt
+#SBATCH -e /home/mstetter/BGS_sims/logs/simulation_stderr-%j.txt
 #SBATCH -t 8-00:00
-#SBATCH -J sim_neutralMaizeEuro
+#SBATCH -J bgsSim
 #SBATCH --array=0-199
 #SBATCH --nodes=1-1
 #SBATCH --ntasks 1
@@ -13,4 +13,4 @@
 
 
 mkdir -p results/tmp_neutral
-python sims/sim_humans.py demographies/tennesen.csv results/tmp_neutral $SLURM_ARRAY_TASK_ID
+python sims/sim_humans.py demographies/tennesen.csv results/tmp_neutral/ $SLURM_ARRAY_TASK_ID
